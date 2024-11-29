9e42f363dd13c609b76450031369460148d87766
davidcarlino <david.carlino20@gmail.com>
2024-11-29 22:28:43 +1100

    mac py2app fixed

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+import os
 from setuptools import setup
 
 OPTIONS = {
-    'argv_emulation':False,
-
+    'argv_emulation': False,
+    'iconfile': os.path.abspath('./iconmac.icns'),  # Use absolute path
 }
 
 setup(
-    app=["main-mac.py"],
-    setup_requires=["py2app"],
+    app=["main-mac.py"],               # Your main script
+    options={"py2app": OPTIONS},       # Pass OPTIONS dictionary to py2app
+    setup_requires=["py2app"],         # py2app setup requirement
 )
